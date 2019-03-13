eac8b05c901156bee68ae449080783cce521d250
jiongfei.li <jiongfei.li@ly.com>
2019-03-13 15:47:34 +0800

    测试用户账号

--- litemall-all/src/main/java/org/linlinjava/litemall/Application.java
+++ litemall-all/src/main/java/org/linlinjava/litemall/Application.java
@@ -14,6 +14,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class Application {
 
     public static void main(String[] args) throws Exception {
+        //测试
         SpringApplication.run(Application.class, args);
     }
 
